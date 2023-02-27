304d24f60831c3e9f4b943449cfe59bb4cb94a4d
digpatel123 <digpatel123@gmail.com>
2023-02-27 17:11:23 +0000

    Using event-listeners to perform random drawing with the Turtle.

---
+++ main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+
+tt = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tt.forward(20)
+
+
+def move_back():
+    tt.backward(20)
+
+
+def turn_right():
+    tt.right(5)
+
+
+def turn_left():
+    tt.left(5)
+
+
+def clear():
+    tt.clear()
+    tt.penup()
+    tt.home()
+    tt.penup()
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="s", fun=move_back)
+screen.onkey(key="c", fun=clear)
+
+screen.exitonclick()
